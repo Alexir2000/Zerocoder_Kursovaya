7e6c58faa9304287dbb1419cfecf3c1f14c65c98
Alexir2000 <hit2007@list.ru>
2024-07-28 22:55:58 +0300

    синхро Orders

--- flowers_shop/orders/models.py
+++ flowers_shop/orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from main.models import Users, Tovar
 
-# Create your models here.
+class CartItem(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    tovar = models.ForeignKey(Tovar, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.tovar.Nazvanie} (x{self.quantity})'
--- flowers_shop/orders/views.py
+++ flowers_shop/orders/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import CartItem
+from flowers_shop.main.models import Tovar
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def add_to_cart(request, tovar_id):
+    tovar = Tovar.objects.get(id=tovar_id)
+    cart_item, created = CartItem.objects.get_or_create(user=request.user, tovar=tovar)
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()
+    return redirect('cart_detail')
+
+@login_required
+def cart_detail(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    return render(request, 'orders/cart_detail.html', {'cart_items': cart_items})
