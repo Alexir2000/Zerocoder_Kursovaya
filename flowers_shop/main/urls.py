d63c494a8f49df11be01e177786ccff60f5ef945
Alexir2000 <hit2007@list.ru>
2024-08-04 10:05:09 +0300

    синхро

--- flowers_shop/main/urls.py
+++ flowers_shop/main/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views  # Импорт стандартных представлений аутентификации
 from . import views
-from .views import register, CustomLoginView
+from .views import register, CustomLoginView, custom_logout_view
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html', next_page='index'), name='login'),  # Используйте стандартное представление
-    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),  # Используйте стандартное представление выхода
+    path('logout/', custom_logout_view, name='logout'),  # Используйте кастомное представление для выхода
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='main/password_reset.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='main/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='main/password_reset_confirm.html'), name='password_reset_confirm'),
