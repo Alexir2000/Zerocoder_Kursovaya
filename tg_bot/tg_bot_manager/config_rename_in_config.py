cf163a453205ab91fea4456ea96cad0dfe558203
Alexir2000 <hit2007@list.ru>
2024-08-09 22:21:31 +0300

    синхро

---
+++ tg_bot/tg_bot_manager/config_rename_in_config.py
@@ -0,0 +1,12 @@
+
+TOKEN = 'Ваш токен Телеграм'
+URL_BASE = "http://127.0.0.1:8000"
+
+URL_API_GET_ZAKAZ = "http://127.0.0.1:8000/catalog/catalog_put_zakaz"
+URL_API_OK_PUT_RESPONSE = "http://127.0.0.1:8000/catalog/Ok_put_response"
+
+URL_API_GET_ZHURNAL_STATUS = "http://127.0.0.1:8000/catalog/put_zhurnal_status"
+URL_API_OK_PUT_ZHURNAL_RESPONSE = "http://127.0.0.1:8000/catalog/Ok_put_zhurnal_response"
+
+URL_API_GET_ANALITICS = "http://127.0.0.1:8000/analytics/put_analytics"
+
